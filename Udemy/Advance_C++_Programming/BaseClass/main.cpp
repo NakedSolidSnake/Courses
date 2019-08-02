7d39006b0025318b1529f10cedd9eb5e601a42b9
Cristiano silva de Souza <cristianosstec@gmail.com>
2019-08-01 22:46:45 -0300

    Derived Class

--- Udemy/Advance_C++_Programming/BaseClass/main.cpp
+++ Udemy/Advance_C++_Programming/BaseClass/main.cpp
@@ -42,14 +42,33 @@ class Employee{
     }
 };
 
+class Manager : public Employee{
+  private:
+    bool salaried;
+
+  public:
+    Manager(string name, double payRate, bool isSalaried)
+      : Employee(name, payRate)
+    {
+      this->salaried = isSalaried;
+    }
+
+    bool getSalaried() const {
+      return this->salaried;
+    }
+};
+
 int main()
 {
 
-  Employee emp1("Jane Smith", 35000);
-  Employee emp2("Bill Brown", 32000);
+  Employee emp1("Mary Smith", 15.00);
+  cout << "Employee name; " << emp1.getName() << endl;
+  cout << "Employee pay rate: " << emp1.getPay() << endl;
 
-  cout << emp1.toString() << endl;
-  cout << emp2.toString() << endl;
+  Manager emp2("Bob Brown", 1500, true);
+  cout << "Employee name; " << emp2.getName() << endl;
+  cout << "Employee pay rate: " << emp2.getPay() << endl;
+  cout << "Emp2 salaried? " << emp2.getSalaried() << endl;
 
   return 0;
 }
