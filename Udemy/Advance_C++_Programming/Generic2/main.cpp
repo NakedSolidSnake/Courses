c5615965946c9e98e23cc89cfe53d26dfbb69a2b
Cristiano Silva <cristianosstec@gmail.com>
2019-08-05 19:51:24 -0300

    template specialization

--- Udemy/Advance_C++_Programming/Generic2/main.cpp
+++ Udemy/Advance_C++_Programming/Generic2/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+template <typename T>
 class Stack{
   private:
     int datastore[100];
@@ -11,29 +12,65 @@ class Stack{
       top = -1;
     }
 
-    void push(int val){
+    void push(T val){
       ++top;
       datastore[top] = val;
     }
 
-    int pop(void){
-      int val = datastore[top];
+    T pop(void){
+      T val = datastore[top];
+      datastore[top] = 0;  //here it will cause problem if use a string values
+                           //so it is necessary to use template specialization
       --top;
       return val;
     }
 
-    int peek(void){
+    T peek(void){
 
       return datastore[top];
     }
 };
 
+template <>
+class Stack<string>{
+  private:
+    string datastore[100];
+    int top;
+  public:
+    Stack(){
+      top = -1;
+    }
+
+    void push(string val){
+      ++top;
+      datastore[top] = val;
+    }
+
+    string pop(void){
+    string val = datastore[top];
+      datastore[top] = "";
+      --top;
+      return val;
+    }
+
+    string peek(void){
+
+      return datastore[top];
+    }
+};
 int main(){
-  Stack stack;
+ /* Stack<int> stack;
   cout << stack.peek() << endl;
   stack.push(10);
   stack.push(2);
 
+  cout << stack.peek() << endl;
+  stack.pop();
+  cout << stack.peek() << endl;*/
+  Stack<string> stack;
+
+  stack.push("Cristiano");
+  stack.push("Sara");
   cout << stack.peek() << endl;
   stack.pop();
   cout << stack.peek() << endl;
