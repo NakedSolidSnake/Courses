b291dd34ace87195b1888d0d7ed73d720c04bda4
Cristiano Silva <cristianosstec@gmail.com>
2019-10-04 12:47:36 -0300

    arduino rtos

--- books/FreeRtos_A_Practical_Approach_With_Arduino/02-AnalogRead_DigitalRead/02-AnalogRead_DigitalRead.cpp
+++ books/FreeRtos_A_Practical_Approach_With_Arduino/02-AnalogRead_DigitalRead/02-AnalogRead_DigitalRead.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <task.h>
 #include <semphr.h>
 
 typedef struct{
@@ -12,7 +13,7 @@ typedef struct{
 SemaphoreHandle_t xSerialSemaphore;
 
 void TaskDigitalRead(void *pvParameters);
-void TaskAnalogReadParam(void pvParameters);
+void TaskAnalogReadParam(void *pvParameters);
 
 AnalogReadParam_t xParams[3], *pxParam = xParams;
 
@@ -46,8 +47,89 @@ void setup() {
 	pxParam->pcText = "Input A3";
 	pxParam->SensorID = A3;
 	pxParam->DelayTicks = 1; //pdMS_TO_TICKS( 250UL );
+
+	TaskHandle_t xLastTask;
+
+	for(pxParam = &xParams[0]; pxParam < &xParams[3]; pxParam++){
+		Serial.println("\n------------------------------");
+		Serial.print("Creating ");
+		Serial.println(pxParam->pcTaskName);
+
+		xTaskCreate(
+				TaskAnalogReadParam,
+				pxParam->pcTaskName,
+				128,
+				(void *)pxParam,
+				(configMAX_PRIORITIES - 2),
+				&xLastTask
+				);
+	}
+	vTaskSuspend(xLastTask);
+
+	xTaskCreate(
+			TaskDigitalRead,
+			(const portCHAR *)"Digital Read",
+			128,
+			(void *)xLastTask,
+			(configMAX_PRIORITIES - 3),
+			NULL
+			);
+
 }
 
 void loop() {
 
+}
+
+void SerialDebugWithSemaphore(const char *pszText)
+{
+	if( xSemaphoreTake( xSerialSemaphore, (TickType_t) 5) == pdTRUE ){
+		Serial.println(pszText);
+		xSemaphoreGive(xSerialSemaphore);
+	}
+}
+
+void TaskDigitalRead(void *pvParameters)
+{
+	uint8_t pushButton = 2;
+	TaskHandle_t xTaskParaReiniciar = (TaskHandle_t) pvParameters;
+
+	pinMode(pushButton, INPUT);
+
+	for( ; ; ){
+		int buttonState = digitalRead(pushButton);
+
+		if (xSemaphoreTake(xSerialSemaphore, (TickType_t)5) == pdTRUE){
+			Serial.print("digital input: ");
+
+			Serial.println(buttonState);
+
+			xSemaphoreGive(xSerialSemaphore);
+
+			if(xTaskParaReiniciar != NULL && (buttonState == 1)){
+				vTaskResume(xTaskParaReiniciar);
+			}
+		}
+		vTaskDelay(1);
+	}
+}
+
+void TaskAnalogReadParam(void *pvParameters)
+{
+	AnalogReadParam_t *pxParams = (AnalogReadParam_t *)pvParameters;
+
+	for(;;){
+
+		int sensorValue = analogRead(pxParams->SensorID);
+
+		if (xSemaphoreTake(xSerialSemaphore, (TickType_t) 5) == pdTRUE){
+			Serial.print(pxParams->pcText);
+			Serial.print(": ");
+			Serial.println(sensorValue);
+
+			xSemaphoreGive(xSerialSemaphore);
+		}
+
+		vTaskDelay(pxParams->DelayTicks);
+	}
 }
