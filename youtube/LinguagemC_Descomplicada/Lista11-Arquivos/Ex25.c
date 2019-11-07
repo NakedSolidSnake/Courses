ed073fdb0e2da07bc7ea1ed08abca4983813f9d6
Cristiano silva de Souza <cristianosstec@gmail.com>
2019-11-07 08:25:18 -0300

    ex25-partial

--- youtube/LinguagemC_Descomplicada/Lista11-Arquivos/Ex25.c
+++ youtube/LinguagemC_Descomplicada/Lista11-Arquivos/Ex25.c
@@ -22,13 +22,15 @@ struct Registers
 };
 
 static int Register_insert(void);
-static int Register_remove(struct Register *reg);
+static int Register_remove(const char *name);
 static int Register_find(const char *name, struct Register *reg);
 static int Register_listAll(void);
 static int Register_listFirstLetter(char letter);
 static int Register_printBirthday();
+static int verifyRegister(struct Register *reg);
 
 static int menu(void);
+static int selectMenu(char sel);
 static int loadFile(const char *file);
 static int saveAll(void);
 
@@ -66,21 +68,37 @@ static int Register_insert(void)
   if(!ret){
     rgs.reg[rgs.nreg] = r;
     rgs.nreg++;
+    return 0;
   }
 
+  return -1;
 }
 
-static int Register_remove(struct Register *reg)
+static int Register_remove(const char *name)
 {
+  for(int i = 0; i < rgs.nreg; i++){
+    struct Register *r = &rgs.reg[i];
+    if(!strcmp(name, r->name)){
+      memset(r->name, '\0', MAX_NAME);
+      memset(r->phone, '\0', MAX_PHONE);
+      r->day = 0;
+      r->month = 0;
+      return 0;
+    }
+  }  
 
+  return -1;
 }
 
 static int Register_find(const char *name, struct Register *reg)
 {
+  char n[MAX_NAME] = {0};
+  memcpy(n, name, strlen(name));
+  n[strlen(n) - 1] = '\0';
   for(int i = 0; i < rgs.nreg; i++){
-    struct Register *r = &rgs.reg[rgs.nreg];
-    if(!strcmp(name, r->name)){
-      reg = r; 
+    struct Register *r = &rgs.reg[i];
+    if(!strcmp(n, r->name)){
+      memcpy(reg, r, sizeof(struct Register));
       return 0;
     }
   }  
@@ -90,17 +108,29 @@ static int Register_find(const char *name, struct Register *reg)
 
 static int Register_listAll(void)
 {
-
+  for(int i = 0; i < rgs.nreg; i++){
+    struct Register *r = &rgs.reg[i];
+    printf("NAME: %s PHONE: %s DAY: %d MONTH: %d\n", r->name, r->phone, r->day, r->month);
+  }  
+  return 0;
 }
 
 static int Register_listFirstLetter(char letter)
 {
+  for(int i = 0; i < rgs.nreg; i++){
+    struct Register *r = &rgs.reg[i];
+    if(r->name[0] == letter){
+      printf("NAME: %s PHONE: %s DAY: %d MONTH: %d\n", r->name, r->phone, r->day, r->month);
+    }
+  }  
+
+  return 0;
 
 }
 
 static int Register_printBirthday()
 {
-
+  return 0;
 }
 
 static int menu(void)
@@ -116,10 +146,78 @@ static int menu(void)
     printf("[6] - Print all month Birthday \n");
     printf("[q] - Exit program \n");
 
-    choose = getchar();
-  }while(choose != 'q');
+    scanf("%c", &choose);
+    getchar();
+
+    system("clear");
+
+  }while(!selectMenu(choose));
 
   saveAll();
+  return 0;
+}
+
+static int selectMenu(char sel)
+{
+  char name[MAX_NAME];
+  struct Register reg;
+  int ret;
+  int fret;
+  switch(sel){
+    case 'q':
+      printf("Quitting...\n");
+      ret = 1;
+      break;
+
+    case '1':
+      printf("Insert Data.\n");
+      ret = 0;
+      break;
+
+    case '2':
+      printf("Remove Data.\n");
+      ret = 0;
+      break;
+
+    case '3':
+      printf("Find Contact by name: \n");
+      printf("Enter a name: ");
+      fgets(name, MAX_NAME, stdin);
+      fret = Register_find(name, &reg);
+      if(fret != -1){
+        printf("NAME: %s PHONE: %s DAY: %d MONTH: %d\n", reg.name, reg.phone, reg.day, reg.month );
+      }else{
+        printf("Not found.\n");
+      }
+
+      printf("\n");
+
+      ret = 0;
+      break;
+
+    case '4':
+      printf("List all Contacts.\n");
+      Register_listAll();
+      printf("\n");
+      ret = 0;
+      break;
+
+    case '5':
+      printf("List Contacts by first letter.\n");
+      ret = 0;
+      break;
+      
+    case '6':
+      printf("Print all by Birthday.\n");
+      ret = 0;
+      break;
+
+    default:
+      printf("Select an existing option.\n");
+      ret = 0;
+  }
+
+  return ret;
 }
 
 static int loadFile(const char *file)
@@ -171,11 +269,34 @@ static int saveAll(void)
   }
   else{
     for(int i = 0; i < rgs.nreg; i++){
-      struct Register *reg = &rgs.reg[rgs.nreg];
-      fprintf("NAME: %s PHONE: %s DAY: %d MONTH: %d\n", reg->name, reg->phone, reg->day, reg->month );
+      struct Register *reg = &rgs.reg[i];
+      if(strlen(reg->name) == 0)
+        continue;
+      fprintf(fd,"NAME: %s PHONE: %s DAY: %d MONTH: %d\n", reg->name, reg->phone, reg->day, reg->month );
     }
   }
 
   fclose(fd);
+  return 0;
+}
+
+static int verifyRegister(struct Register *reg)
+{
+  if(strlen(reg->name) == 0){
+    return 1;
+  }
+
+  if(strlen(reg->phone) == 0){
+    return 1;
+  }
+
+  if(reg->day <= 0){
+    return 1;
+  }
+
+  if(reg->month <= 0){
+    return 1;
+  }
+
   return 0;
 }
